26d53849240b4cbf7fd924053cd92669cca82f97
Thamalpath <thamalpathsathimantha3@gmail.com>
2026-01-16 13:25:02 +0530

    Remove unused controller and route files for products, categories, departments, and subcategories

--- src/routes/departments.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const c = require("../controllers/departments.controller");
-
-router.get("/", c.list);
-router.get("/:id", c.getById);
-
-module.exports = router;
--- src/routes/subcategories.routes.js
+++
@@ -1,7 +0,0 @@
-const router = require("express").Router();
-const c = require("../controllers/subcategories.controller");
-
-router.get("/", c.list);
-router.get("/:id", c.getById);
-
-module.exports = router;
